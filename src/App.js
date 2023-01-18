e1812c17e9910b9696b1125ca33b785bacd91f5f
Meenakshi Lodhi Rajput <rajputmeenakshi982@gmail.com>
2023-01-18 22:43:49 +0530

    completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button } from "react-bootstrap";
+import { useState } from "react";
 
 function App() {
+  const [bio, setBio] = useState("");
+  const [upperCaseBio, setUpperCaseBio] = useState("");
+
+  const enteredText = (e) => {
+    setBio(e.target.value);
+  };
+
+  const handleClick = () => {
+    return setUpperCaseBio(bio.toUpperCase());
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App container justify-content-center align-items-center ">
+      <h1 className="row justify-content-center align-items-center">Enter Introduction/Bio</h1> <br/>
+      <textarea className="row justify-content-center align-items-center w-100 " onChange={enteredText}></textarea> <br />
+      <div className="text-center">
+      <Button onClick={handleClick}>Change Text</Button> <br />
+      </div>
+      <p className="row justify-content-center align-items-center">{upperCaseBio}</p>
     </div>
   );
 }
